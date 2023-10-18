38b8be095f1fef8027acf082de0060614ead696b
AnjanaPasinduMadushan <pbapmadushan@gmail.com>
2023-10-18 23:59:01 +0530

    changes

---
+++ lib/model/souvenir_model.dart
@@ -0,0 +1,11 @@
+class Souvenir {
+  final String shopName;
+  final String address;
+  final String description;
+
+  Souvenir({
+    required this.shopName,
+    required this.address,
+    required this.description,
+  });
+}
