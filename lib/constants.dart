10de2e2e05458b0069d38ab9376c086fce979da5
Priyantha <87350551+Priyantha-IT21021534@users.noreply.github.com>
2023-10-19 01:12:32 +0530

    feature: InputField (#4)

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,12 @@
-class Images{
-  static const logo = 'assets/images/logo.png';
-  static const slogan = 'assets/images/slogan.png';
+import 'package:flutter/material.dart';
+
+class MyImages{
+  static const String  logo = 'assets/images/logo.png';
+  static const String  slogan = 'assets/images/slogan.png';
   static const iconLogo = 'assets/images/logo_img.png';
+}
+
+class MyColors{
+  static const Color pink = Color(0xFFFF5A6E);
+  static const Color ash = Color(0xFF444452);
 }
