f9f4db6cd31fac6f1887f51817447290c7ab7114
Hesara Pasan <hesaramahendra123@gmail.com>
2023-10-27 23:27:19 +0530

    top nav bar changed

--- lib/pages/preferences_page.dart
+++ lib/pages/preferences_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:tour_me/constants.dart';
 import 'package:tour_me/widgets/pink_button.dart';
+import 'package:tour_me/widgets/top_nav.dart';
 
 class PreferencesPage extends StatefulWidget {
   static const String routeName = '/preferences';
@@ -38,25 +39,8 @@ class _PreferencesPageState extends State<PreferencesPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: Padding(
-          padding: const EdgeInsets.only(left: 10),
-          child: Image.asset(
-            MyImages.iconLogo,
-          ),
-        ),
-        actions: const [
-          Padding(
-            padding: EdgeInsets.only(right: 10),
-            child: CircleAvatar(
-              backgroundImage: NetworkImage("https://i1.sndcdn.com/artworks-000076772850-gfnyiy-t240x240.jpg"),
-              radius: 20,
-            ),
-          ),
-        ],
-        backgroundColor: Colors.black,
-        elevation: 16,
-      ),
+      appBar: const TopNav(),
+      
       body: Container(
         decoration: const BoxDecoration(gradient: MyColors.backgrounGradient),
         // alignment: Alignment.,
